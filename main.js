c4e7fe2fc49ced86cf99f48772bb3c1dfbc1df5e
Nelson Lumpkin <ossocasa@gmail.com>
2023-03-20 15:18:13 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (crazyNumber) => {
+        const quarter = crazyNumber / 4
+        return quarter
+
+}
+const numberReturned = quarterValue(2002020)
+// Invoke the function and store the return value
+
+
+// Log the return value
+console.log(numberReturned)
